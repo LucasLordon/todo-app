ebc4153d2271bafafc2e5b600ebf32cad520d251
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-12-02 09:16:42 +0100

    feat(auth): Test the login of a created user

--- backend/routes/auth.test.js
+++ backend/routes/auth.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const router = require('./auth.api.js')
 const express = require('express')
+const bcrypt = require('bcrypt');
 const UserModel = require('../database/models/user.model');
 
 const app = express();
@@ -14,6 +15,19 @@ describe('Authentication of an user', () =>  {
             .send({email : "hello@world.ch", password: "123"})
             .then(res => {
                 expect(res.body).toEqual("Utilisateur non trouvé")
+            })
+    })
+    it('should return a user logged in', async () => {
+        const user = new UserModel({
+            email: "hello@world.ch",
+            password: await bcrypt.hash("12345678", 8),
+          });
+          user.save()
+        await request(app)
+            .post('/')
+            .send({email: user.email, password: "12345678"})
+            .then(res => {
+                expect(res.body.email).toEqual(user.email)
             })
     })
 })
