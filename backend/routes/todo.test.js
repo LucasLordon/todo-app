1f8c68d87ec090e4dcde3e7740b07cbfc1a1b918
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-12-16 10:43:29 +0100

    feat(todoTest): Create tests to delete a todo and add a todo

---
+++ backend/routes/todo.test.js
@@ -0,0 +1,77 @@
+const request = require('supertest')
+const router = require('./todo.api.js')
+const express = require('express')
+const bcrypt = require('bcrypt');
+const UserModel = require('../database/models/user.model.js');
+const jsonwebtoken = require('jsonwebtoken')
+const { key } = require('../env/keys');
+
+const app = express();
+app.use(express.json())
+app.use(router);
+
+
+describe('Manage a todo', () => {
+  it('should create a todo', async () => {
+    const user = new UserModel({
+      email: "hello@world.ch",
+      password: await bcrypt.hash("12345678", 8),
+    });
+    await user.save()
+
+    const token = jsonwebtoken.sign({}, key, {
+      subject: user._id.toString(),
+      expiresIn: 60 * 60 * 24 * 30 * 6,
+      algorithm: 'RS256',
+    });
+
+    request(app)
+      .post('/add')
+      .set('Cookie', ["token=" + token])
+      .send({ text: "My first todo" })
+      .then(res => {
+        expect(res.status).toEqual(200)
+      })
+  })
+
+  it('should change the status of a todo', async () => {
+    const user = new UserModel({
+      email: "hello@world.ch",
+      password: await bcrypt.hash("12345678", 8),
+    });
+    await user.save()
+
+    const token = jsonwebtoken.sign({}, key, {
+      subject: user._id.toString(),
+      expiresIn: 60 * 60 * 24 * 30 * 6,
+      algorithm: 'RS256',
+    });
+    // Adds a new todo
+    request(app)
+      .post('/add')
+      .set('Cookie', ["token=" + token])
+      .send({ text: "My first todo" })
+      .then(res => {
+        expect(res.status).toEqual(200)
+      })
+
+    let todos;
+    // Finds
+    request(app)
+      .get('/')
+      .set('Cookie', ["token=" + token])
+      .then(res => {
+        expect(res.status).toEqual(200)
+        todos = res.body
+        console.log(todos)
+      })
+    // deletes
+    request(app)
+      .post('/' + todos[0]._id)
+      .set('Cookie', ["token=" + token])
+      .then(res => {
+        expect(res.status).toEqual(200)
+      })
+  })
+})
+
